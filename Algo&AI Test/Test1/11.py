4e3d978b1bc9bcf880d1166005a955e7f6c2a644
craxy14 <xxmarselxx5@gmail.com>
2025-02-22 16:59:41 +0400

    second commit

---
+++ Algo&AI Test/Test1/11.py
@@ -0,0 +1,63 @@
+# ფუნქცია რომელსაც გადაჰყავს რიცხვი ორობითი სისტემიდან ათობითში
+def decimal(num):
+    num = str(num)[::-1]
+    result = 0
+    
+    for i in range(len(num)):
+        if num[i] == "1":
+            result += 2 ** i
+    
+    return result
+    
+print(decimal(100101))
+print(decimal(1101))
+print(decimal(10010111))
+
+#------------------------------------------------------------------
+
+# ფუნქცია რომელსაც გადაჰყავს რიცხვი ათობითი სისტემიდან ორობითში
+def binary(num):
+    result = ""
+    
+    while num > 0:
+        result += str(num % 2)
+        num = num // 2
+    
+    return result[::-1]
+    
+print(binary(234))
+print(binary(46))
+print(binary(145))
+
+#------------------------------------------------------------------
+
+# ფუნქცია რომელსაც გადაჰყავს რიცხვი ათობითი სისტემიდან რვაობითში
+def rvaobiti(num):
+    result = ""
+    
+    while num > 0:
+        result += str(num % 8)
+        num = num // 8
+        
+    return result
+    
+print(rvaobiti(54))
+print(rvaobiti(134))
+
+#------------------------------------------------------------------
+
+# ფუნქცია რომელსაც გადაჰყავს რიცხვი რვაობითი სისტემიდან ათობითში
+def rvaobiti_to_atobiti(num):
+    num = str(num)[::-1]
+    res = 0
+
+    for i in range(len(num)):
+        res += (8 ** i) * int(num[i])
+    return res
+
+print(rvaobiti_to_atobiti(234))
+print(rvaobiti_to_atobiti(74))
+print(rvaobiti_to_atobiti(94))
+print(rvaobiti_to_atobiti(38))
+
+#------------------------------------------------------------------
